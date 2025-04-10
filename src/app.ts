ea9c88b686bcaa220ec14e9dd8afcb3bd1f06971
DanielNwolu <nwoludaniel.c@gmail.com>
2025-04-10 19:00:44 +0100

    implemented the cors allowed origins and added the frontend url

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
-/// File: src/app.ts
+// File: src/app.ts
 import express from 'express';
 import { json } from 'body-parser';
+import cors from 'cors';
+import { corsOptions } from './config/cors';
 import { loggerMiddleware } from './middleware';
 import userRouter from './modules/user/routes';
 import authRouter from './modules/auth/route';
 
-
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(json());
 app.use(loggerMiddleware);
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-// Register routes here (e.g. /api/customers)
 export default app;
