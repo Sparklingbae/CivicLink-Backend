119282f542b350718b473b895112c24a0c7715dc
novice000 <maxwellefemena@gmail.com>
2025-04-12 21:29:27 +0100

    added all cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,6 +4,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://civic-link-fe-temp-jog1.vercel.app",
   "https://civic-link-again.vercel.app",
+  "*",
+  // allowing * will allow all origins to access the API
 ];
 
 export const corsOptions: CorsOptions = {
