5567063591e6ec91366748111d50f8094f7fba6d
novice000 <maxwellefemena@gmail.com>
2025-04-12 03:13:51 +0100

    updated officials model, validator and interfaces

--- src/modules/officials/interfaces.ts
+++ src/modules/officials/interfaces.ts
@@ -12,73 +12,75 @@ export interface Location {
     localGovernment: string;
 }
 
-export enum Title {
-    // Federal Executive
-    President = 'President',
-    VicePresident = 'Vice President',
-    SecretaryToTheGovernment = 'Secretary to the Government of the Federation',
-    HeadOfService = 'Head of Civil Service of the Federation',
-    Minister = 'Minister',
-    SpecialAdviser = 'Special Adviser',
-    SeniorSpecialAssistant = 'Senior Special Assistant',
-    SpecialAssistant = 'Special Assistant',
+// export enum Title {
+//     // Federal Executive
+//     President = 'President',
+//     VicePresident = 'Vice President',
+//     SecretaryToTheGovernment = 'Secretary to the Government of the Federation',
+//     HeadOfService = 'Head of Civil Service of the Federation',
+//     Minister = 'Minister',
+//     SpecialAdviser = 'Special Adviser',
+//     SeniorSpecialAssistant = 'Senior Special Assistant',
+//     SpecialAssistant = 'Special Assistant',
   
-    // Federal Legislature
-    Senator = 'Senator',
-    HouseOfRepMember = 'House of Representatives Member',
-    SpeakerHouseOfReps = 'Speaker of the House of Representatives',
-    SenatePresident = 'Senate President',
-    DeputySenatePresident = 'Deputy Senate President',
+//     // Federal Legislature
+//     Senator = 'Senator',
+//     HouseOfRepMember = 'House of Representatives Member',
+//     SpeakerHouseOfReps = 'Speaker of the House of Representatives',
+//     SenatePresident = 'Senate President',
+//     DeputySenatePresident = 'Deputy Senate President',
   
-    // State Executive
-    Governor = 'Governor',
-    DeputyGovernor = 'Deputy Governor',
-    StateCommissioner = 'Commissioner',
-    SecretaryToStateGovernment = 'Secretary to the State Government',
-    HeadOfServiceState = 'Head of Civil Service (State)',
-    SpecialAdviserState = 'State Special Adviser',
-    SpecialAssistantState = 'State Special Assistant',
+//     // State Executive
+//     Governor = 'Governor',
+//     DeputyGovernor = 'Deputy Governor',
+//     StateCommissioner = 'Commissioner',
+//     SecretaryToStateGovernment = 'Secretary to the State Government',
+//     HeadOfServiceState = 'Head of Civil Service (State)',
+//     SpecialAdviserState = 'State Special Adviser',
+//     SpecialAssistantState = 'State Special Assistant',
   
-    // State Legislature
-    StateAssemblyMember = 'State House of Assembly Member',
-    SpeakerStateAssembly = 'Speaker of the State House of Assembly',
+//     // State Legislature
+//     StateAssemblyMember = 'State House of Assembly Member',
+//     SpeakerStateAssembly = 'Speaker of the State House of Assembly',
   
-    // Local Government
-    LocalGovernmentChairman = 'Local Government Chairman',
-    LocalGovernmentViceChairman = 'Local Government Vice Chairman',
-    Councillor = 'Councillor',
+//     // Local Government
+//     LocalGovernmentChairman = 'Local Government Chairman',
+//     LocalGovernmentViceChairman = 'Local Government Vice Chairman',
+//     Councillor = 'Councillor',
   
-    // Judiciary
-    ChiefJustice = 'Chief Justice of Nigeria',
-    Justice = 'Justice',
-    Judge = 'Judge',
-    Magistrate = 'Magistrate',
-    AttorneyGeneral = 'Attorney General',
+//     // Judiciary
+//     ChiefJustice = 'Chief Justice of Nigeria',
+//     Justice = 'Justice',
+//     Judge = 'Judge',
+//     Magistrate = 'Magistrate',
+//     AttorneyGeneral = 'Attorney General',
   
-    // Traditional & Ceremonial
-    Emir = 'Emir',
-    Oba = 'Oba',
-    Obi = 'Obi',
-    Igwe = 'Igwe',
-    Sultan = 'Sultan',
-    Chancellor = 'Chancellor',
+//     // Traditional & Ceremonial
+//     Emir = 'Emir',
+//     Oba = 'Oba',
+//     Obi = 'Obi',
+//     Igwe = 'Igwe',
+//     Sultan = 'Sultan',
+//     Chancellor = 'Chancellor',
   
-    // General/Other
-    Chairman = 'Chairman',
-    Honorable = 'Honorable',
-    DirectorGeneral = 'Director General',
-    PermanentSecretary = 'Permanent Secretary',
-    Director = 'Director',
-    Coordinator = 'Coordinator',
-}
+//     // General/Other
+//     Chairman = 'Chairman',
+//     Honorable = 'Honorable',
+//     DirectorGeneral = 'Director General',
+//     PermanentSecretary = 'Permanent Secretary',
+//     Director = 'Director',
+//     Coordinator = 'Coordinator',
+// }
   
 
 export interface Official {
     _id: ObjectId;
     name: string;
-    title: Title | string; // Use Title enum or string for flexibility
+    title: string; // Use Title enum or string for flexibility
+    img_url?: string;
     level: ObjectId; // Reference to Government Level
     ministry?: string; // Optional ministry field
+    jurisdiction?: string;
     responsibility_area: string;
     contact_info: ContactInfo;
     location: Location;
@@ -90,7 +92,7 @@ export interface Official {
 export interface OfficialDocument extends mongoose.Document, Official {
     _id: mongoose.Types.ObjectId; // Override _id to match mongoose.Document
     name: string;
-    title: Title;
+    title: string;
     level: ObjectId; // Reference to Government Level
     ministry?: string; // Optional ministry field
     responsibility_area: string;
@@ -103,7 +105,7 @@ export interface OfficialDocument extends mongoose.Document, Official {
 }
 export interface OfficialRequest { 
     name: string;
-    title: Title;
+    title: string;
     level: ObjectId; // Reference to Government Level
     ministry?: string; // Optional ministry field
     responsibility_area: string;
@@ -113,7 +115,7 @@ export interface OfficialRequest {
 }
 export interface OfficialResponse {
     name: string;
-    title: Title;
+    title: string;
     level: ObjectId; // Reference to Government Level
     ministry?: string; // Optional ministry field
     responsibility_area: string;
@@ -123,7 +125,7 @@ export interface OfficialResponse {
 }
 export interface OfficialUpdateRequest {
     name?: string;
-    title?: Title;
+    title?: string;
     level?: ObjectId; // Reference to Government Level
     ministry?: string; // Optional ministry field
     responsibility_area?: string;
@@ -133,7 +135,7 @@ export interface OfficialUpdateRequest {
 }
 export interface OfficialUpdateResponse {  
     name?: string;
-    title?: Title;
+    title?: string;
     level?: ObjectId; // Reference to Government Level
     ministry?: string; // Optional ministry field
     responsibility_area?: string;
--- src/modules/officials/validator.ts
+++ src/modules/officials/validator.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { BadRequestError } from '../../utilities/errorClasses';
-import {Title} from './interfaces';
+// import {Title} from './interfaces';
 import Joi from 'joi';
 
 
@@ -8,17 +8,52 @@ import Joi from 'joi';
 export const createOfficialValidator = (req: Request, res: Response, next: NextFunction) => {
     const schema = Joi.object({
       name: Joi.string().min(3).max(50).required(),
-      title: Joi.string().valid(...Object.values(Title)).required(),
+      title: Joi.string().required(),
+      img_url: Joi.string().optional(),
       level: Joi.string().required(),
       ministry: Joi.string().optional(),
+      jurisdiction: Joi.string().optional(),
       responsibility_area: Joi.string().max(500).required(),
       contact_info: Joi.object({
-        email: Joi.string().email().required(),
-        phone_number: Joi.string().pattern(/^(\+?\d{1,3}[- ]?)?\d{10}$/).required()
+        email: Joi.string().email().optional(),
+        phone_number: Joi.string().pattern(/^(\+?\d{1,3}[- ]?)?\d{10}$/).optional(),
+        social_media: Joi.string().optional()
       }),
       location: Joi.object({
-        state: Joi.string().required(),
-        localGovernment: Joi.string().optional()
+        state: Joi.string().optional(),
+        localGovernment: Joi.string().optional(),
+        address: Joi.string().optional()
+      }),
+      active_status: Joi.boolean().optional()
+    });
+  
+    const { error } = schema.validate(req.body, { abortEarly: false });
+  
+    if (error) {
+    throw new BadRequestError(error.details.map(detail => detail.message).join(', '));
+    }
+  
+    next();
+  };
+
+export const updateOfficialValidator = (req: Request, res: Response, next: NextFunction) => {
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(50).optional(),
+      title: Joi.string().optional(),
+      img_url: Joi.string().optional(),
+      level: Joi.string().required(),
+      ministry: Joi.string().optional(),
+      jurisdiction: Joi.string().optional(),
+      responsibility_area: Joi.string().max(500).optional(),
+      contact_info: Joi.object({
+        email: Joi.string().email().optional(),
+        phone_number: Joi.string().pattern(/^(\+?\d{1,3}[- ]?)?\d{10}$/).optional(),
+        social_media: Joi.string().optional()
+      }),
+      location: Joi.object({
+        state: Joi.string().optional(),
+        localGovernment: Joi.string().optional(),
+        address: Joi.string().optional()
       }),
       active_status: Joi.boolean().optional()
     });
